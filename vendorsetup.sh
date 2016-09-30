c7adfeb607a88a7705e7fbb81a721f5b77cd472e
Alvin Francis <nivlafx@gmail.com>
2016-09-29 21:06:36 -0400

    7.0
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/nfxosp/grunt/master/nfx-build-targets | sed -e 's/#.*$//' | grep nfx-6.0 | awk '{printf "nfx_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/nfxosp/grunt/master/nfx-build-targets | sed -e 's/#.*$//' | grep nfx-7.0 | awk '{printf "nfx_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
